c4033cc1c68e40087f40e85c4e4ef6f3b37160a2
kervines <kervinzero@gmail.com>
2024-05-25 00:12:11 -0300

    feat: validaçao do check box para envio

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+const checkInput = document.querySelector('#check');
+const submit = document.querySelector('input[type="button"]');
+
+console.log(submit);
+
 function handleClick() {
-  alert('Enviou');
+  if (
+    checkInput.getAttributeNames().filter((att) => att === 'checked') ==
+    'checked'
+  ) {
+    alert('Enviou');
+  }
 }
+
+checkInput.addEventListener('click', () => {
+  checkInput.toggleAttribute('checked');
+});
+
+submit.addEventListener('click', handleClick);
