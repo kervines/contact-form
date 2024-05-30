ac79459d009a45d9433091dd40f28c9232d19332
kervines <kervinzero@gmail.com>
2024-05-29 23:16:12 -0300

    feat - validation form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-import initSubmit from './submit.js';
+const inputFName = document.querySelector('.fname');
+const inputLName = document.querySelector('.lname');
+const inputEmail = document.querySelector('.email');
+const inputRadio1 = document.querySelector('.query1');
+const inputRadio2 = document.querySelector('.query2');
+const inputTextArea = document.querySelector('.msg');
+const inputCheckBox = document.querySelector('.checkbox');
 
-initSubmit();
+const msgRequired = document.querySelector('.msg-required');
