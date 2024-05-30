bed95981e37b00530f1985b1867aba931d1f6f1d
kervines <kervinzero@gmail.com>
2024-05-30 18:38:55 -0300

    feat - validação de inputs

--- js/app.js
+++ js/app.js
@@ -10,9 +10,50 @@ const inputCheckBox = document.querySelector('.checkbox');
 // Mensagem de erro
 const msgRequired = document.querySelectorAll('.msg-required');
 
+const validationEmail = () => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = inputEmail.value;
+  return regex.test(email);
+};
+
+const validation = () => {
+  msgRequired.forEach((msg) => {
+    if (inputFName.value === '') {
+      if (msg.dataset.info === inputFName.className) {
+        msg.classList.remove('display-none');
+        inputFName.classList.add('bd-red');
+        setTimeout(() => {
+          msg.classList.add('display-none');
+          inputFName.classList.remove('bd-red');
+        }, 2000);
+      }
+    }
+
+    if (inputLName.value === '') {
+      if (msg.dataset.info === inputLName.className) {
+        msg.classList.remove('display-none');
+        inputLName.classList.add('bd-red');
+        setTimeout(() => {
+          msg.classList.add('display-none');
+          inputLName.classList.remove('bd-red');
+        }, 2000);
+      }
+    }
+
+    if (inputEmail.value === '' || !validationEmail()) {
+      if (msg.dataset.info === inputEmail.className) {
+        msg.classList.remove('display-none');
+        inputEmail.classList.add('bd-red');
+        setTimeout(() => {
+          msg.classList.add('display-none');
+          inputEmail.classList.remove('bd-red');
+        }, 2000);
+      }
+    }
+  });
+};
+
 // Button
-const buttonSubmit = document.querySelector('.submit');
+const buttonSubmit = document.querySelector('input[type="button"]');
 
-const validationInputs = () => {};
-
-buttonSubmit.addEventListener('click', validationInputs);
+buttonSubmit.addEventListener('click', validation);
